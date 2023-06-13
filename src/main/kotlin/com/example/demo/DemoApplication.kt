e325704a715a18375229c28b383d5f018be9d2be
lesincs <lesincs923@gmail.com>
2023-06-14 06:37:16 +0800

    log b3 headers

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,10 +1,27 @@
 package com.example.demo
 
+import brave.sampler.Sampler
+import org.springframework.boot.actuate.autoconfigure.tracing.TracingProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
+
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication{
+	@Bean
+	fun restTemplate(builder: RestTemplateBuilder): RestTemplate? {
+		// Do any additional configuration here
+		return builder.build()
+	}
+
+	@Bean
+	fun braveSampler(properties: TracingProperties): Sampler {
+		return Sampler.ALWAYS_SAMPLE
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
--- src/main/kotlin/com/example/demo/hello/HelloController.kt
+++ src/main/kotlin/com/example/demo/hello/HelloController.kt
@@ -1,16 +1,28 @@
 package com.example.demo.hello
 
+import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
-    private val logger = LoggerFactory.getLogger(HelloController::class.java)
+    private val logger: Logger = LoggerFactory.getLogger(HelloController::class.java)
 
-    @GetMapping("/hello")
-    fun hello(): String {
+    @Autowired
+    private lateinit var httpRequest: HttpServletRequest
+
+    @GetMapping("/helloworld")
+    fun helloWorld(): String {
         logger.info("Received request for /hello")
+        logger.info("http headers:${httpRequest.headerNames.toList()}")
+        for (headerName in httpRequest.headerNames) {
+            if (headerName == "x-b3-traceid" || headerName == "x-b3-spanid" || headerName == "b3") {
+                logger.info("http header: " + headerName + ":" + httpRequest.getHeader(headerName))
+            }
+        }
         return "helloworld"
     }
 }
